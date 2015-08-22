1737026118dd57efb8b7e3cd86c660d439c63b10
Ryan Tablada <ryan.tablada@gmail.com>
2015-08-22 13:53:22 -0500

    Update's divshot for autodeploy

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('dashboard', {path: '/'}, function() {
+
+  });
+
+  this.route('login');
+  this.route('logout');
 });
 
 export default Router;
